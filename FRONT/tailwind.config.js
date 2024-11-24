bf397e86c62ee10060771b5fe916337cbf6ce6f3
zakialounas-deliverycar <zakia.lounas@deliverycar.fr>
2024-11-25 00:10:58 +0100

    Initial commit: Transfer project to my academic repository

--- FRONT/tailwind.config.js
+++ FRONT/tailwind.config.js
@@ -1,11 +1,9 @@
 // tailwind.config.js
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Inclus tous les fichiers dans src
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
 
   plugins: [],
-}
+};
